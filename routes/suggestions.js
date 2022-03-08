1dd37677be0cd0c3ed3d69518b260afeaa93852e
John A Godwin <johngodwinakaboivado@gmail.com>
2022-03-08 14:42:33 +0100

    change the implementation on the suggestions files.

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const { Suggestion, validate } = require('../models/suggestion')
 const { Category,  } = require('../models/category')
-const { Status } = require('../models/status')
 
 
 
 const router = express.Router()
 
 router.get('/', async(req, res) => {
-  const suggestions = await Suggestion.find().populate('status').populate('category')
+  const suggestions = await Suggestion.find().populate('category')
   res.send(suggestions)
 })
 
@@ -18,11 +17,6 @@ router.post('/', async (req, res) => {
 
   const category = await Category.findById(req.body.categoryId)
   if(!category) return res.status(400).send("invalid category")
-
-  // const status = await Status.findById(req.body.statusId)
-  // console.log(req.body.statusId)
-  // if(!status) return res.status(400).send("invalid status")
-
 
   
   const suggestion = new Suggestion({
@@ -41,29 +35,39 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.message)
-  const categoty = await Category.findByIdAndUpdate(req.params.id, { title: req.body.title }, { new: true })
+  const category = await Category.findById(req.body.categoryId)
+  if (!category) return res.status(404).send('ivalid category')
+
+  const suggestion = await Suggestion.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    description: req.body.description,
+    upvotes: req.body.upvotes,
+    status: req.body.status,
+    category: category._id
+  }
+  , { new: true })
   
-  if (!categoty) return res.status(404).send('category with the given Id not found')
-  
-  await categoty.save()
-  res.send(categoty)
+ 
+  if (!suggestion) return res.status(400).send('suggestion with the given id not found.')
+  await suggestion.save()
+  res.send(suggestion)
 })
 
 
 router.get('/:id', async (req, res) => {
-  const categoty = await Category.findById(req.params.id)
-  if (!categoty) return res.status(404).send('category with the given Id not found')
+  const suggestion = await Suggestion.findById(req.params.id).populate('category')
+  if (!suggestion) return res.status(404).send('category with the given Id not found')
 
-  res.send(categoty)
+  res.send(suggestion)
 })
 
 
 router.delete('/:id', async (req, res) => {
 
-  const categoty = await Category.findByIdAndDelete(req.params.id)
-  if (!categoty) return res.status(404).send('category with the given Id not found')
+  const suggestion = await Suggestion.findByIdAndDelete(req.params.id)
+  if (!suggestion) return res.status(404).send('category with the given Id not found')
 
-  res.send(categoty)
+  res.send(suggestion)
 })
 
 module.exports = router
