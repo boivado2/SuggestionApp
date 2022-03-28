8c7a02ef77edcef11b3aac89ebc34c4504594d6c
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-03-28 15:44:57 +0100

    change the email property to username  the auth route.

--- routes/auth.js
+++ routes/auth.js
@@ -12,20 +12,20 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.message)
 
-  let user = await User.findOne({ email: req.body.email })
+  let user = await User.findOne({ username: req.body.username })
   if (!user) return res.status(400).send('invalid email or password.')
   
   const validPassword = await bcrypt.compare(req.body.password, user.password)
   if (!validPassword) return res.status(400).send('invalid email or password')
   
   const token = user.generateAuthToken()
-  res.send(token)
+  res.json(token)
 })
 
 
 const validate = (user) => {
   const schema =  Joi.object({
-    email: Joi.string().min(5).max(225).required().email(),
+    username: Joi.string().min(5).max(225).required(),
     password: Joi.string().min(4).max(225).required(),    
   })
    
