c7815a55667c061761137690e0494371fbb919f8
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-03-28 13:57:40 +0100

    deleted model/reply file.

--- routes/comments.js
+++ routes/comments.js
@@ -75,63 +75,6 @@ router.delete('/comments/:id', async (req, res) => {
 
 })
 
-// get all replies associated with a given comment
-router.get('/replies/:id', async(req, res) => {
-  const replies = await Reply.find({commentId: req.params.id})
-  res.send(replies)
-})
-
-
-// reply a comment
-router.post('/replies/:commentId', async (req, res) => {
-    const session = await mongoose.startSession()
-
-    const { error } = validateReply(req.body)
-    if (error) return res.status(400).send(error.message)
-    const comment = await Comment.findById(req.params.commentId).session(session)
-  if (!comment) return res.status(400).send("invalid comment")
-    const user = await User.findById(req.body.userId)
-    if(!user) return res.status(400).send("invalid User")
-    
-    const reply = new Reply({
-      content: req.body.content,
-      replyingTo: "@" + comment.user.username,
-      user: {
-        _id: user._id,
-        image_url: user._image_url,
-        username: user.username,
-        email: user.email
-      },
-      suggestionId:comment.suggestionId,
-      commentId: comment._id
-
-    }, { session: session })
-    
-      await session.withTransaction(async () => {
-        await comment.replies.push(reply)
-        await reply.save({session})
-        await comment.save({session})
-        res.send(reply)
-
-  })
-      session.endSession()
-
-
-})
-
-
-router.delete('/replies/:replyId', async (req, res) => {
-
-    const reply = await Reply.findById(req.params.replyId)
-    if (!reply) return res.status(404).send("reply with the given id not found.")
-      
-    await Reply.deleteOne({ commentId: reply.commentId, replyId: req.params.replyId })
-    res.send("deleted Successfully")
-    
-
-  
-})
-
 
 
 
