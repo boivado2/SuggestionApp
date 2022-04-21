db155b5741192f157a0ebe3b3df618a79a2a1c79
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-04-21 13:10:31 +0100

    all generated tokens to expires.

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
 
 
 userSchema.methods.generateAuthToken = function() {
- return jsonwebtoken.sign({_id: this._id}, config.get('feedPrivatekey'))
+ return jsonwebtoken.sign({_id: this._id}, config.get('feedPrivatekey'), {expiresIn: '1h'})
 }
 
 const User = mongoose.model('User', userSchema)
